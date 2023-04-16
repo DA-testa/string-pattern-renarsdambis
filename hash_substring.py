2bfc71b2838b8e6333e5f13cc8ccf5c7a21e755c
renarsdambis <125255526+renarsdambis@users.noreply.github.com>
2023-04-16 23:39:07 +0000

    ko

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,48 @@
-# python3
-
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    # read two lines: pattern and text
+    pattern = input().rstrip()
+    text = input().rstrip()
+    return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
+    # print the occurrences
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    # implementation of Rabin-Karp algorithm to find occurrences of pattern in text
+    p = 10**9 + 7  # prime number for hash calculation
+    x = 263  # base for hash calculation
+    
+    n = len(text)
+    m = len(pattern)
+    pattern_hash = hash(pattern, p, x)
+    text_hashes = precompute_hashes(text, m, p, x)
+    
+    occurrences = []
+    for i in range(n-m+1):
+        if pattern_hash != text_hashes[i]:
+            continue
+        if text[i:i+m] == pattern:
+            occurrences.append(i)
+    return occurrences
+    
+def hash(s, p, x):
+    h = 0
+    for c in reversed(s):
+        h = (h*x + ord(c)) % p
+    return h
 
-    # and return an iterable variable
-    return [0]
-
-
-# this part launches the functions
+def precompute_hashes(text, m, p, x):
+    n = len(text)
+    h = [0] * (n-m+1)
+    s = text[n-m:]
+    h[n-m] = hash(s, p, x)
+    y = 1
+    for i in range(m):
+        y = (y*x) % p
+    for i in range(n-m-1, -1, -1):
+        h[i] = (x*h[i+1] + ord(text[i]) - y*ord(text[i+m])) % p
+    return h
+    
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
